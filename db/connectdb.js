b616155110099749f32ddacd4208074933ead223
mariamiqrar17 <mariam_iqrar_intern@netixsol.com>
2023-12-27 06:20:19 +0500

    change

---
+++ db/connectdb.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// * This is the mongodb Atlas connection link
+const dbConnect = process.env.db_link;
+
+// * This is the mongodb Atlas connection
+mongoose.connect(dbConnect).then(() => {
+
+    console.log('Hurrah! MongoDB connection successfully established :)');
+
+}).catch((err) => {
+
+    console.log('Sorry Bro! MongoDB is not connected :(', err.message);
+
+})
